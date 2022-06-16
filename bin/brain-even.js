41da5b0305f8ce11621ecfbd8d761f32a98173e9
johngalt <rustam.minhairoff@yandex.ru>
2022-06-16 17:47:06 +0300

    ''

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import gameCondition from '../src/even.js';
+import userName from '../src/cli.js';
 
-console.log(`Hello ${gameCondition}!`);
+console.log(`Hello ${userName}!`);
