9320972c26142d35081c1a0587ec0a74d7e5b688
johngalt <rustam.minhairoff@yandex.ru>
2022-07-12 20:18:37 +0300

    ....

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 import userName from '../src/cli.js';
 
-console.log(`Hello ${userName}!`);
-
 import { evenGame } from '../src/even.js';
+
+console.log(`Hello ${userName}!`);
 
 evenGame();
