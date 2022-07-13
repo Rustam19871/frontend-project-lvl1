9772e9a465fcade1a03e0dc21d8d4ba09c888231
johngalt <rustam.minhairoff@yandex.ru>
2022-07-13 18:59:45 +0300

    ///

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,5 +1,10 @@
 #!/usr/bin/env node
-import userName from '../src/cli.js';
+import readlineSync from 'readline-sync';
+
+// eslint-disable-next-line no-console
+console.log('Welcome to the Brain Games!');
+
+const userName = readlineSync.question('May I have your name? ');
 
 // eslint-disable-next-line import/named
 import { evenGame } from '../src/even.js';
