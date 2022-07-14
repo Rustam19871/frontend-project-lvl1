a0368b84497b35075966d2f1f4b691be51e6ee2b
johngalt <rustam.minhairoff@yandex.ru>
2022-07-14 19:25:00 +0300

    /////

--- src/even.js
+++ src/even.js
@@ -1,25 +1,42 @@
 import readlineSync from 'readline-sync';
 
-// Определение функции, дающее рандомное целое число
-const getRandomInt = (max) => Math.floor(Math.random() * max);
+const roundsCount = 3;
+// edit rounds count
 
-// Определение функции игры на четность
-// eslint-disable-next-line consistent-return, import/prefer-default-export
-export const evenGame = () => {
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  for (let i = 0; i < 3; i += 1) {
-    const number = getRandomInt(100);
-    console.log('Question: ', number);
-    const answerUser = readlineSync.question('Your answer: ');
-    const checking1 = (number % 2 === 0) && (answerUser === 'yes');
-    const checking2 = (number % 2 !== 0) && (answerUser === 'no');
-    if (checking1 === true || checking2 === true) {
-      console.log('Correct!');
-    } else {
-      const opposite = (answerUser === 'yes' ? 'no' : 'yes');
-      const errorMessage = `"${answerUser}" is wrong answer ;(. Correct answer was '${opposite}'. /n Let's try again, ${userName}!`;
-      return errorMessage;
+const engine = (gameRules, gameCheck) => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log(gameRules);
+
+  for (let i = 1; i <= roundsCount; i += 1) {
+    const [getQuestion, correctAnswer] = gameCheck();
+    console.log(`Question: ${getQuestion}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (userAnswer !== correctAnswer) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
     }
+    console.log('Correct!');
   }
   console.log(`Congratulations, ${userName}!`);
 };
+
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const gameRules = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+const isEven = (number) => number % 2 === 0;
+
+const gameCheck = () => {
+  const number = getRandomNumber(1, 100);
+  const question = `${number}`;
+  const correctAnswer = isEven(number) ? 'yes' : 'no';
+  return [question, correctAnswer];
+};
+
+const evenGame = () => engine(gameRules, gameCheck);
+
+export default evenGame;
