d87daf205d52ad9f2206cf477a77a26773c6aa8f
johngalt <rustam.minhairoff@yandex.ru>
2022-07-14 17:49:28 +0300

    ...

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,9 +1,8 @@
 #!/usr/bin/env node
+console.log('Welcome to the Brain Games!');
 
 // eslint-disable-next-line no-console
 // eslint-disable-next-line import/named
 import { greeting } from '../src/even.js';
-
-console.log('Welcome to the Brain Games!');
 
 greeting();
